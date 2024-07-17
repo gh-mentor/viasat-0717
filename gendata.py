862626cc2a074b41a38a29755e063fb326d37b0c
Andrew C. Scoppa <andrew-scoppa@github.com>
2024-07-17 12:48:38 -0400

    Update

---
+++ gendata.py
@@ -0,0 +1,8 @@
+"""
+This app uses Python 3.12, numpy, and pandas to generate a set of data points and plot them on a graph.
+"""
+
+# import the required libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt

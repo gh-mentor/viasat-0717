51c295752e9269d5a6da230e94e41e24bf84a2aa
Andrew C. Scoppa <andrew-scoppa@github.com>
2024-07-17 11:52:07 -0400

    Update

---
+++ inventory.sql
@@ -0,0 +1,12 @@
+/*
+This file contains a script of Transact SQL (T-SQL) command to interact with a database named 'Inventory'.
+Requirements:
+- SQL Server 2022 is installed and running
+- database 'Inventory' already exists.
+Details:
+- Sets the default database to 'Inventory'.
+- Creates a 'categories' table and related 'products' table if they do not already exist.
+- Remove all rows from the tables (in case they already existed).
+- Populates the 'Categories' table with sample data.
+- Populates the 'Products' table with sample data.
+*/

bb6a738b4403c149c2d7f105ff07f3687b6b8e14
Andrew C. Scoppa <andrew-scoppa@github.com>
2024-07-17 13:09:35 -0400

    Update

--- combinatorics.cpp
+++ combinatorics.cpp
@@ -25,6 +25,18 @@ fact(4) => 24
 fact(8) => 40320
 */
 
+
+
+/**
+ * @brief Calculates the factorial of a given number.
+ * 
+ * @example fact(0) => 1
+ * @example fact(1) => 1
+ * @example fact(4) => 24
+ * @example fact(8) => 40320
+ * @param n The number for which factorial is to be calculated.
+ * @return The factorial of the given number.
+ */
 unsigned long long fact(unsigned int n) {
     // base case
     if (n == 0 || n == 1) {
@@ -32,4 +44,20 @@ unsigned long long fact(unsigned int n) {
     }
     // recursive case
     return n * fact(n - 1);
+}
+
+/**
+ * @brief Calculates the permutation of n and r.
+ * 
+ * @example perm(5, 2) => 20
+ * @example perm(6, 3) => 120
+ * @param n The total number of elements.
+ * @param r The number of elements to be selected.
+ * @return The permutation of n and r.
+ */
+unsigned long long perm(unsigned int n, unsigned int r) {
+    // check if r is greater than n
+    assert(r <= n);
+    // return the permutation
+    return fact(n) / fact(n - r);
 }
